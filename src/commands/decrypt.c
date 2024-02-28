0d3190ff29780fc97c58b7aafb59a11fc39b527a
ExOP17 <matmog10@gmail.com>
2024-02-28 14:49:57 +0100

    ajout de la commande decrypt

---
+++ src/commands/decrypt.c
@@ -0,0 +1,21 @@
+#include "../utils/utils.h"
+#include "../utils/global_variable.h"
+#include <inttypes.h>
+// extern RsaKey global_key;
+// extern SecretData global_sd;
+
+uint8_t cmd_decrypt(uint8_t * pt, uint8_t len)
+{
+    trigger_high();
+    big_int res = decrypt(global_sd.ct, global_key.d, global_key.n);
+
+	uint8_t d_as_bytes[64] = {0};
+    sprintf(d_as_bytes, "pt=%lu", res);
+
+    set_global_sd_ct(res);
+   	simpleserial_put('r', 64, d_as_bytes);
+    trigger_low();
+
+	// https://chipwhisperer.readthedocs.io/en/latest/simpleserial.html
+    return 0;
+}
